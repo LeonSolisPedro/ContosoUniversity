6f7971a8ec0d08eac53ec1dd305896dcc07c8ac6
LeonSolisPedro <pedro@wintercr.com>
2023-12-11 20:05:49 -0800

    Added Diagnostics.EntityFrameworkCore and configured it on Program.cs, I documented everything there with comments...

--- Data/SchoolContext.cs
+++ Data/SchoolContext.cs
@@ -7,12 +7,24 @@ using ContosoUniversity.Models;
 
 namespace ContosoUniversity.Data
 {
+    //Microsoft call this "Database context class", so be it.
+    //Weirdly, this is also called Service
+    //So it needs to be registered in Program.cs
     public class SchoolContext : DbContext
     {
         public SchoolContext(DbContextOptions options) : base(options)
         {
         }
 
-        public virtual DbSet<Student> Student { get; set; } = default!;
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().ToTable("Course");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            modelBuilder.Entity<Student>().ToTable("Student");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -4,18 +4,24 @@ using ContosoUniversity.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// The AddDatabaseDeveloperPageExceptionFilter, allows to show different error pages.
+// For migrations
 builder.Services.AddRazorPages();
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDbContext<SchoolContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("SchoolContext")));
-
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+else
+{
+    //This thingy, allows to apply migrations on the fly. wowser!
+    //As long as you run dotnet ef migrations add Initial
+    app.UseDeveloperExceptionPage();
+    app.UseMigrationsEndPoint();
 }
 
 app.UseHttpsRedirection();
